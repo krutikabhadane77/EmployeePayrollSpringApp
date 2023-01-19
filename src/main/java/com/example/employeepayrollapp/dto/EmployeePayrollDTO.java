f572fcfd19860fd5d2ba954b6b2b99d3dd468f1e
krutikabhadane77 <krutikabhadane588@gmail.com>
2023-01-19 15:01:08 +0530

    Added: UC1 Use lombok library to auto generate getters and setters for the DTO

--- src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,16 +1,8 @@
 package com.example.employeepayrollapp.dto;
+import lombok.Data;
 
+@Data
 public class EmployeePayrollDTO {
     public String name;
     public long salary;
-
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "name = " + name + " salary = " + salary;
-    }
 }
